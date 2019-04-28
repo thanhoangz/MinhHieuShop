aeace59ce33442072a117b6eaf8651e993c5d5d1
ThanHoang <ThanHoang@DESKTOP-K0913AQ>
2019-04-28 10:22:33 +0700

    Phân trang thành công

--- MinhHieuShop.Web/Api/ProductCategoryController.cs
+++ MinhHieuShop.Web/Api/ProductCategoryController.cs
@@ -3,7 +3,9 @@ using MinhHieuShop.Model.Models;
 using MinhHieuShop.Service;
 using MinhHieuShop.Web.Infrastructure.Core;
 using MinhHieuShop.Web.Models;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
@@ -13,7 +15,7 @@ namespace MinhHieuShop.Web.Api
     [RoutePrefix("api/productcategory")]
     public class ProductCategoryController : ApiControllerBase
     {
-        IProductCategoryService _productCategoryService;
+        private IProductCategoryService _productCategoryService;
 
         public ProductCategoryController(IErrorService errorService, IProductCategoryService productCategoryService)
             : base(errorService)
@@ -22,13 +24,27 @@ namespace MinhHieuShop.Web.Api
         }
 
         [Route("getall")]
-        public HttpResponseMessage GetAll(HttpRequestMessage request)
+        public HttpResponseMessage GetAll(HttpRequestMessage request, int page, int pageSize = 20)
         {
             return CreateHttpResponse(request, () =>
             {
+                int totalRow = 0;
                 var model = _productCategoryService.GetAll();
-                var responseData = Mapper.Map<IEnumerable<ProductCategory>, IEnumerable<ProductCategoryViewModel>>(model);
-                var response = request.CreateResponse(HttpStatusCode.OK, responseData);
+
+                totalRow = model.Count();
+
+                var query = model.OrderBy(x => x.ID).Skip(page * pageSize).Take(pageSize);  // theo tiêu chí Id tăng dần
+
+                var responseData = Mapper.Map<IEnumerable<ProductCategory>, IEnumerable<ProductCategoryViewModel>>(query);
+
+                var paginationSet = new PaginationSet<ProductCategoryViewModel>()
+                {
+                    Items = responseData,
+                    Page = page,
+                    TotalCount = totalRow,
+                    TotalPages = (int)Math.Ceiling((decimal)totalRow / pageSize)
+                };
+                var response = request.CreateResponse(HttpStatusCode.OK, paginationSet);
                 return response;
             });
         }
