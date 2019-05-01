3ad09a3ea93715a5cc6b8d4141bd39579ba85f19
ThanHoang <ThanHoang@DESKTOP-K0913AQ>
2019-05-01 12:47:27 +0700

    ok xong xóa

--- MinhHieuShop.Web/app/components/product_categories/productCategoryListController.js
+++ MinhHieuShop.Web/app/components/product_categories/productCategoryListController.js
@@ -1,9 +1,9 @@
 ﻿(function (app) {
     app.controller('productCategoryListController', productCategoryListController);
 
-    productCategoryListController.$inject = ['$scope', 'apiService', 'notificationService'];
+    productCategoryListController.$inject = ['$scope', 'apiService', 'notificationService','$ngBootbox'];
 
-    function productCategoryListController($scope, apiService, notificationService) {
+    function productCategoryListController($scope, apiService, notificationService, $ngBootbox) {
         $scope.productCategories = [];
         $scope.page = 0;
         $scope.pagesCount = 0;
@@ -11,10 +11,28 @@
         $scope.keyword = '';
 
         $scope.search = search;
+        $scope.deleteProductCategory = deleteProductCategory;
+
+        function deleteProductCategory(id) {
+            $ngBootbox.confirm('Bạn có chắc muốn xóa?').then(function () {
+                var config = {
+                    params: {
+                        id: id
+                    }
+                }
+                apiService.del('api/productcategory/delete', config, function () {
+                    notificationService.displaySuccess('Xóa thành công');
+                    search();
+                }, function () {
+                    notificationService.displayError('Xóa không thành công');
+                })
+            });
+        }
 
         function search() {
             getProductCagories();
         }
+
         function getProductCagories(page) {
             page = page || 0;
             var config = {
--- MinhHieuShop.Web/app/shared/modules/minhhieushop.common.js
+++ MinhHieuShop.Web/app/shared/modules/minhhieushop.common.js
@@ -1,3 +1,3 @@
 ﻿(function () {
-    angular.module('minhhieushop.common', ['ui.router'])
+    angular.module('minhhieushop.common', ['ui.router','ngBootbox'])
 })();
