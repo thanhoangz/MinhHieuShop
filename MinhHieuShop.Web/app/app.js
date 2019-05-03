445b23d37574ce67d473bf74a60acfeac8317a78
ThanHoang <ThanHoang@DESKTOP-K0913AQ>
2019-05-03 23:01:04 +0700

    có gì đó k ổn ở nút sản phẩm

--- MinhHieuShop.Web/app/app.js
+++ MinhHieuShop.Web/app/app.js
@@ -1,4 +1,5 @@
 ﻿/// <reference path="../assets/admin/libs/angular/angular.js" />
+
 (function () {
     angular.module('minhhieushop',
         [
@@ -10,11 +11,22 @@
     config.$inject = ['$stateProvider', '$urlRouterProvider'];
 
     function config($stateProvider, $urlRouterProvider) {
-        $stateProvider.state('home', {
-            url: "/admin",
-            templateUrl: "/app/components/home/homeView.html",
-            controller: "homeController"
-        });
-        $urlRouterProvider.otherwise('/admin');
+        $stateProvider
+            .state('base', {
+                url: '',
+                templateUrl: '/app/shared/views/baseView.html',
+                abstract: true
+            }).state('login', {
+                url: "/login",
+                templateUrl: "/app/components/login/loginView.html",
+                controller: "loginController"
+            })
+            .state('home', {
+                url: "/admin",
+                parent: 'base',
+                templateUrl: "/app/components/home/homeView.html",
+                controller: "homeController"
+            });
+        $urlRouterProvider.otherwise('/login');
     }
 })();
